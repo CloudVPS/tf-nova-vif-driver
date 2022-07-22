501fc3dfa7e0ff8c5e505612eba741a478d766e3
bkranendonk <43269299+bkranendonk@users.noreply.github.com>
2022-07-22 15:10:43 +0200

    Move vrouter-port-control

--- setup.py
+++ setup.py
@@ -25,10 +25,10 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=requirements('requirements.txt'),
     entry_points={
-      'vrouter-port-control' : ['tools.vrouter_port_control:main'],
+      'console_scripts': ['vrouter-port-control = vif_plug_vrouter.vrouter_port_control:main'],
       'os_vif': [
         'contrail_vrouter = vif_plug_contrail_vrouter.vrouter:VrouterPlugin',
-        'vrouter = vif_plug_vrouter.vrouter:VrouterPlugin'
+        'vrouter = vif_plug_vrouter.vrouter:VrouterPlugin',
       ]
     },
     zip_safe=False,
